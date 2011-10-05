cf71ae1dffcc0394d8946fe35b3af4128118bf73
David Demers <ddemers@etelephonesystems.com>
2011-10-05 20:09:18 -0300

    Edited app/overrides/add_google_base_to_config_menu.rb via GitHub

--- app/overrides/add_google_base_to_config_menu.rb
+++ app/overrides/add_google_base_to_config_menu.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => 'admin/configurations/index',
                      :name => 'add google base to config menu',
-                     :insert_before => "[data-hook='admin_configurations_menu']",
-                     :text => %q{<%= configurations_menu_item('Google Base', admin_google_base_settings_path, 'Google Base taxonomy management') %>})
+                     :insert_bottom => "[data-hook='admin_configurations_menu']",
+                     :text => %q{<%= configurations_menu_item('Google Base', admin_google_base_settings_path, 'Google Base taxonomy management') %>}
+                     :disabled => false)
