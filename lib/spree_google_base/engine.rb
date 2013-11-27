48c1b67854ba05c15549472604f908b2eeb7c622
David Demers <fonemstr@gmail.com>
2013-11-27 12:25:57 -0500

    Add shipping weight to product attributes

--- lib/spree_google_base/engine.rb
+++ lib/spree_google_base/engine.rb
@@ -6,7 +6,7 @@ module SpreeGoogleBase
 
     initializer "spree.google_base.environment", :before => :load_config_initializers do |app|
       Spree::GoogleBase::Config = Spree::GoogleBaseConfiguration.new
-      
+
       # See http://support.google.com/merchants/bin/answer.py?hl=en&answer=188494#US for all other fields
       SpreeGoogleBase::FeedBuilder::GOOGLE_BASE_ATTR_MAP = [
         ['g:id', 'id'],
@@ -14,6 +14,7 @@ module SpreeGoogleBase
         ['title', 'google_base_title'],
         ['description', 'google_base_description'],
         ['g:price', 'price'],
+        ['g:shipping_weight', 'weight'],
         ['g:condition', 'google_base_condition'],
         ['g:product_type', 'google_base_taxon_type'],
         ['g:google_product_category', 'google_base_product_category'],
@@ -30,6 +31,6 @@ module SpreeGoogleBase
     end
 
     config.to_prepare &method(:activate).to_proc
-    
+
   end
 end
